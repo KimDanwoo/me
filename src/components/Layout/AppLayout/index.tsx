d1e5f0b0150dcebe3f7b7a4ba29285bc1a9c44ff
danwoo <dudgh9198@naver.com>
2023-07-01 15:54:36 +0900

    :art: 레이아웃 수정

--- src/components/Layout/AppLayout/index.tsx
+++ src/components/Layout/AppLayout/index.tsx
@@ -1,33 +1,35 @@
-import type { PropsWithChildren } from "react";
-import React from "react";
-import { useInView } from "react-intersection-observer";
-import { Link } from "gatsby";
-import "~/styles/components/layout.scss";
+import type { PropsWithChildren } from 'react'
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
+import { Link } from 'gatsby'
+import '~/styles/components/layout.scss'
 
 const AppLayout = ({ children }: PropsWithChildren) => {
-  const [ref, isScrollTop] = useInView({ initialInView: true });
+  const [ref, isScrollTop] = useInView({ initialInView: true })
 
   return (
     <>
       <div ref={ref} />
-      <div className="layout-wrapper">
-        <header className={!isScrollTop ? "scrolled" : undefined}>
-          <Link className="link-to-home" to="/">
-            Dansoon
-          </Link>
-          <a className="link-to-about" href="https://github.com/KimDanwoo">
-            GITHUB
-          </a>
+      <div className='layout-wrapper'>
+        <header className={!isScrollTop ? 'scrolled' : undefined}>
+          <div>
+            <Link className='link-to-home' to='/'>
+              Dansoon
+            </Link>
+            <a className='link-to-about' href='https://github.com/KimDanwoo'>
+              GITHUB
+            </a>
+          </div>
         </header>
         <main>{children}</main>
         <footer>
-          ©<a href="https://github.com/KimDanwoo">Danwoo</a>, Built with
+          ©<a href='https://github.com/KimDanwoo'>Danwoo</a>, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href='https://www.gatsbyjs.com'>Gatsby</a>
         </footer>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default AppLayout;
+export default AppLayout
---
+++ src/pages/me/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const IntroPage = () => {
+  return <div>하이루루</div>
+}
+
+export default IntroPage
