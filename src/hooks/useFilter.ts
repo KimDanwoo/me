41579c666d856e8428fbcbc7890ec6ce479a1f05
KimDanwoo <dudgh9198@naver.com>
2024-01-19 17:04:02 +0900

    ✨ 검색기능 개선 작업중

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,51 @@
+import uniq from 'lodash/uniq'
+import { useMemo, useState } from 'react'
+
+export default function useFilter(posts: Common.Post[]) {
+  const [category, setCategory] = useState<string>('ALL')
+  const [search, setSearch] = useState<string>('')
+
+  const filteredPosts = (posts: Common.Post[]) => {
+    const filteredPosts = posts.filter(p => p.title !== '🧑🏻‍💻 frontend 김단우')
+    if (category === 'ALL' && search === '') {
+      return filteredPosts
+    }
+
+    if (category === 'ALL' && search !== '') {
+      return filteredPosts.filter(p =>
+        p.title.toLowerCase().includes(search.toLowerCase())
+      )
+    }
+
+    if (category !== 'ALL' && search === '') {
+      return filteredPosts.filter(p => p.category === category)
+    }
+
+    return filteredPosts
+      .filter(p => p.category === category)
+      .filter(p => p.title.toLowerCase().includes(search.toLowerCase()))
+  }
+
+  const categories = useMemo(
+    () => ['ALL', ...uniq(posts.filter(p => p.category).map(p => p.category))],
+    [posts]
+  )
+
+  const handleClickCategory = (tag: string) => {
+    if (tag) {
+      setCategory(tag)
+    }
+  }
+
+  const onClickSearch = (text: string) => {
+    setSearch(text)
+  }
+
+  return {
+    category,
+    categories: search !== '' ? [] : categories,
+    handleClickCategory,
+    filteredPosts: filteredPosts(posts),
+    onClickSearch
+  }
+}
